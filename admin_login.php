0e79fdf8a31c932ac517b5f94fecf97e36d92a56
Gihan Punarji <gihanpunarji@Gihans-MacBook-Pro.local>
2022-12-21 22:02:01 +0530

    Admin Login Desin Completed

--- admin_login.php
+++ admin_login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student sign up</title>
+    <title>Admin Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- CUSTOM CSS -->
@@ -13,18 +13,13 @@
 </head>
 
 <body>
-    <div class="login_bg d-none d-md-block">
-        <div class="bg_overlay">
-        </div>
-    </div>
     <div class="container">
         <div class="col-12">
             <div class="row">
                 <div class="col-12">
                     <div class="main-form">
-                        <div class="mt-4 pt-4">
-                            <h1 class="title text-center text-black">Welcome to the student management
-                                system
+                        <div class="title mt-4 pt-4">
+                            <h1 class="text-center text-black text-md-white">Welcome to the student management system
                             </h1>
                         </div>
                         <!-- Input Email -->
@@ -32,20 +27,20 @@
                             <input type="email" placeholder="Enter your email for varification." class="form-control">
                         </div>
                         <!-- Button -->
-                        <div class="d-flex justify-content-center mt-3">
-                            <div class="d-grid col-6 mx-auto">
-                                <button class="login_btn btn btn-primary">
-                                    Verify Email
-                                </button>
+
+                        <p class="text-end me-4 mt-2 admin-btn">Verify Email</p>
+
+                        <!-- Admin Verification Code -->
+
+                        <div class="col-12">
+                            <div class="form-group ms-4 me-4 mt-2">
+                                <input disabled type="text" placeholder="Enter verification Code." class="form-control">
                             </div>
                         </div>
-                        <!-- Register Text -->
-                        <div class="reg-text mt-4 text-end me-4">
-                            <span>If you are already a student <a href="student_login.php">click here</a></span>
-                        </div>
-                        <!-- Message Text -->
-                        <div class="col-12 mt-5">
-                            <p id="message" class="text-center text-info">hello</p>
+                        <div class="d-flex justify-content-center">
+                            <div class="mx-auto d-grid mt-3 col-12">
+                                <button disabled class="btn btn-primary me-4 ms-4">Proceed</button>
+                            </div>
                         </div>
                     </div>
                 </div>
