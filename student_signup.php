f6acf806f314355fc8ce5c46eb0de55fd03e27bf
Gihan Punarji <gihanpunarji@Gihans-MacBook-Pro.local>
2022-12-21 20:49:37 +0530

    student login UI uodated

--- student_signup.php
+++ student_signup.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Signup</title>
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- CUSTOM CSS -->
@@ -13,9 +13,6 @@
 </head>
 
 <body>
-
-    <!-- Background Image -->
-
     <div class="login_bg">
         <div class="bg_overlay">
         </div>
@@ -25,52 +22,30 @@
             <div class="row">
                 <div class="col-12">
                     <div class="main-form">
-                        <!-- Logo -->
-                        <div class="logo d-flex justify-content-center align-items-center mt-2">
-                            <img src="resources/user.png">
+                        <div class="title mt-4 pt-4">
+                            <h1 class="text-center text-white">Welcome to the student management system</h1>
                         </div>
-                        <!-- Form Group -->
-                        <div class="form-groups mt-5">
-                            <div class="form-group ms-4 me-4">
-                                <input type="email" name="email" class="form-control " type="text"
-                                    placeholder="Enter Email" required="" aria-required="true">
-                            </div>
-
-                            <div class="form-group ms-4 mt-4 me-4">
-                                <input type="password" name="password" class="form-control " type="text"
-                                    placeholder="Enter Password" required="" aria-required="true">
-                            </div>
-                        </div>
-                        <!-- Remember Me And Forgot Password -->
-                        <div class="col-12 w-100 d-flex align-items-center justify-content-between mt-4">
-                            <div class="remember-me ms-5">
-                                <input type="checkbox" name="" id="rm">
-                                <label for="rm">Remember Me</label>
-                            </div>
-                            <div class="forgot_pwd me-5">
-                                <a href="#">Forgot Password</a>
-                            </div>
+                        <!-- Input Email -->
+                        <div class="form-group ms-4 me-4 mt-5 pt-4">
+                            <input type="email" placeholder="Enter your email for varification." class="form-control">
                         </div>
                         <!-- Button -->
-                        <div class="d-flex justify-content-center mt-4">
-                            <button class="login_btn btn btn-primary">
-                                Login
-                            </button>
+                        <div class="d-flex justify-content-center mt-3">
+                            <div class="d-grid col-6 mx-auto">
+                                <button class="login_btn btn btn-primary">
+                                    Verify Email
+                                </button>
+                            </div>
                         </div>
                         <!-- Register Text -->
-                        <div class="reg-text">
-
+                        <div class="reg-text mt-4 text-end me-4">
+                            <span>If you are already a student <a href="student_login.php">click here</a></span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
