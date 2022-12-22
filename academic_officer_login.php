f56998108c6d3956806c423363f7553496e3fb5e
Gihan Punarji <gihanpunarji@Gihans-MacBook-Pro.local>
2022-12-22 14:35:36 +0530

    Home Page design completed

---
+++ academic_officer_login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Academic Officer's Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-12">
+            <div class="row">
+                <div class="col-12">
+                    <div class="main-form">
+                        <div class="title mt-4 pt-4">
+                            <h1 class="text-center text-black text-md-white">Welcome to the student management system
+                            </h1>
+                        </div>
+                        <!-- Input Email -->
+                        <div class="form-group ms-4 me-4 mt-5 pt-4">
+                            <input type="text" placeholder="Enter your username." class="form-control">
+                        </div>
+                        <div class="form-group ms-4 me-4 mt-2">
+                            <input type="password" placeholder="Enter your password." class="form-control">
+                        </div>
+                        <div class="form-group ms-4 me-4 mt-2">
+                            <input type="text" placeholder="Enter varification code." class="form-control">
+                        </div>
+
+                        <!-- Button -->
+
+                        <div class="d-flex justify-content-center">
+                            <div class="mx-auto d-grid mt-3 col-12">
+                                <button class="btn btn-primary me-4 ms-4">Proceed</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -5,14 +5,63 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Student Management System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
     <?php
     include "db/connection.php";
     ?>
-    <h1>Home</h1>
+
+    <div class="container">
+        <h1 class="text-center mt-3">Welcome to the Student Management System</h1>
+        <div class="col-12">
+            <div class="row mt-5">
+                <div class="col-6 d-none d-md-block">
+                    <div class="home-img"></div>
+                </div>
+                <div class="col-12 col-md-6 text-center d-flex flex-column align-items-center justify-content-center">
+                    <!-- Admin -->
+                    <div class="admin">
+                        <div class="col-12 links-login">
+                            <a href="admin_login.php" class="text-secondary">Admin Login</a>
+                        </div>
+                    </div>
+                    <!-- Academic Officer -->
+                    <div class="academic-officer">
+                        <div class="col-12 links-login">
+                            <a href="academic_officer_login.php" class="text-secondary">Academic Officer
+                                Login</a>
+                        </div>
+                    </div>
+                    <!-- Teacher -->
+                    <div class="teacher">
+                        <div class="col-12 links-login">
+                            <a href="teacher_login.php" class="text-secondary">Teacher Login</a>
+                        </div>
+                    </div>
+                    <!-- Student -->
+                    <div class="student">
+                        <div class="col-12 links-login">
+                            <a href="student_login.php" class=" text-secondary">Student</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mt-4 d-block d-md-none">
+                    <div class="home-img"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
